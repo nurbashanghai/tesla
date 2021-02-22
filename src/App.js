01625941b1f411f664ca049dad58941e51b57789
707Anuar <KAVKAz0007>
2021-02-22 13:25:07 +0600

    Anuar add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from "./components/Header/Header";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        Работай давай
-      </header>
+        <Router>
+            <Header/>
+
+            {/*<Switch>*/}
+            {/*    <Route exact path="/" component={Home}/>*/}
+            {/*    <Route exact path="/about" component={About}/>*/}
+            {/*    <Route exact path="/store" component={Store}/>*/}
+            {/*    <Route exact path="/series" component={SeriesHome}/>*/}
+            {/*</Switch>*/}
+        </Router>
     </div>
   );
 }
