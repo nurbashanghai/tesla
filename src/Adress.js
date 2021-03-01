d61c089eec3c7ab97ef0e071ad53d9fe494011de
nurbek <selecid_beka@icloud.com>
2021-03-01 11:21:54 +0600

    ФИНАЛЬНАЯ ВЕРСИЯ 2.0

--- src/Adress.js
+++ src/Adress.js
@@ -1,3 +1,5 @@
-export const API_REGISTRATION = "http://localhost:8000/accounts";
-export const API_CARS = "http://localhost:8000/cars";
-export const API_MERCH = "http://localhost:8000/merch";
+export const API = "https://teslareactnurba.herokuapp.com/home";
+
+export const API_REGISTRATION = `${API}/accounts`;
+export const API_CARS = `${API}/cars`;
+export const API_MERCH = `${API}/merch`;
